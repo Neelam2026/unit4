168cbf6bb9724c403f06c5ec853da0da5f215387
Neelam2026 <97454209+Neelam2026@users.noreply.github.com>
2022-03-20 19:18:29 +0530

    rtt
    jkk

---
+++ assg/src/controller/product.controller.js
@@ -0,0 +1,63 @@
+const express = require("express")
+const router = express.Router();
+const Product=require("../models/product.model");
+const authenticate = require("../middleware/authenticate")
+
+
+router.post("",authenticate, async (req, res)=>{
+try{
+ const product=await Product.create(req.body)
+ return res.status(200).send(product)
+}
+catch(err){
+    return res.status(400).send({message : err.message})  
+}
+});
+
+router.get("", async (req, res) => {
+    try{
+        const product = await Product.find().populate({path:"user_id", select:"name"})
+        return res.status(200).send(product)
+    }
+    catch(err){
+        return res.status(400).send({message : err.message})
+    }
+})
+
+
+router.get("/:id",async(req,res)=>{
+    try{
+  const  product=await Product.findById(req.params.id).populate("user_id").lean().exec();
+  return res.status(200).send(product)
+    }
+    catch(err){
+      return res.status(500).send({message:err.message})
+    }
+  
+  })
+
+router.patch("/:id", async (req, res) => {
+    try {
+      const  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      }).populate({path:"user_id", select:{email:1,password:1}}).exec();
+     
+      return res.status(200).send(product);
+    } catch (err) {
+      return res.status(500).send({ message: err.message });
+    }
+  });
+
+  router.delete("/:id",async(req,res)=>{
+    try{
+      const product=await Product.findByIdAndDelete(req.params.id).lean().exec();
+      return res.status(200).send(product)
+    }
+    catch(err){
+      return res.status(500).send({message:err.message})
+    }
+    
+  })
+
+
+module.exports = router;
